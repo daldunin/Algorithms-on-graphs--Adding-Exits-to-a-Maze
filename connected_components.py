d3ee01b86b034cb0e5308658a3583f34dfb88361
Dmitry Aldunin <d_aldunin@mail.ru>
2019-07-07 23:57:44 +0300

    added problem statement

--- connected_components.py
+++ connected_components.py
@@ -1,5 +1,17 @@
 #Uses python3
 
+# Problem: Adding Exits to a Maze
+# Problem Introduction
+# Now you decide to make sure that there are no dead zones in a maze, that is, that at least one exit is
+# reachable from each cell. For this, you find connected components of the corresponding undirected graph
+# and ensure that each component contains an exit cell.
+# Problem Description
+# Task. Given an undirected graph with 𝑛 vertices and 𝑚 edges, compute the number of connected components
+# in it.
+# Input Format. A graph is given in the standard format.
+# Constraints. 1 ≤ 𝑛 ≤ 103, 0 ≤ 𝑚 ≤ 103.
+# Output Format. Output the number of connected components.
+
 import sys
 import numpy
 
